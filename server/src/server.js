2087032d663abab12b4f3d2efc1595185bbca569
Muhammad Awais <awaisshahid965@gmail.com>
2023-07-15 23:46:54 +0500

    launches test fixed

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,14 @@
 const http = require("http");
 const app = require("./app");
-const mongoose = require("mongoose");
 const { loadPlanetsData } = require("./models/planets.model");
+const { mongoConnect } = require("./services/mongo");
 
 const PORT = process.env.PORT ?? 8000;
 
 const server = http.createServer(app);
 
-const MONGO_URL =
-  "mongodb+srv://awais:11223344@nasaproject.y5tw7av.mongodb.net/?retryWrites=true&w=majority";
-
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connection ready!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.log("MongoDB connection failed!");
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
