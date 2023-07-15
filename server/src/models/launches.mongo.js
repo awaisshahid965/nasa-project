cbe6e8a0bd8954cd5cf4cb91fcd646f3cc174b85
Muhammad Awais <awaisshahid965@gmail.com>
2023-07-15 14:49:50 +0500

    added launches schema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+// Define the schema
+const launchSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: {
+    type: [String],
+    required: true,
+  },
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+const Launch = mongoose.model("Launch", launchSchema);
+
+module.exports = Launch;
